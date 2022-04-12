1463dbe01ca916b8c98dd03fcba32017d6cb25d9
Hengrui-Lyu <lv1027382605@gmail.com>
2022-04-12 12:20:22 +0800

    iteration_1

--- wordpress_iteration1/wp-content/plugins/social-polls-by-opinionstage/assets/gutenberg/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'a62b5cdd19177a5e403e7fa5cbeb2e62');
--- wordpress_iteration1/wp-content/themes/preschool-nursery/sidebar.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
-* The sidebar containing the main widget area
-*/
-?>
-
-<aside id="secondary" class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Blog Sidebar', 'preschool-nursery' ); ?>">
-	<?php dynamic_sidebar( 'sidebox-1' ); ?>
-</aside>
